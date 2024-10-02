342823efa5e8198cff94c2cf03e8128c9a49a0a3
Rakesh G <rakesh.g@mphasis.com>
2024-10-02 12:23:50 +0530

    location post method from web & upload file funct

--- src/main/java/pkg1/pincode/PincodeController.java
+++ src/main/java/pkg1/pincode/PincodeController.java
@@ -1,5 +1,10 @@
 package pkg1.pincode;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +12,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 
 @RestController
 public class PincodeController {
@@ -25,5 +33,21 @@ public class PincodeController {
 	@GetMapping("/pincode/fetch/{pincode}")
 	 public List<PincodeEntity> fetchByPincode(@PathVariable int pincode) {
 		return pr.findByPincode(pincode);
+	}
+	@PostMapping("/upload")
+	public String uploadFile(@RequestParam MultipartFile mpf ) throws IOException {
+		if(mpf.isEmpty()) {
+			return "Please upload a file in String format";
+		}
+		String dir = "uploads/";
+		File  f1 = new File(dir);
+		if(!f1.exists()) {
+			f1.mkdirs();
+		}
+		File f2 = new File(dir);
+		byte[] arr1 = mpf.getBytes();
+		Path path = Paths.get(dir + mpf.getOriginalFilename());
+		Files.write(path, arr1);
+		return "Success";
 	}
 }
--- src/main/java/pkg1/pincode/WebController.java
+++ src/main/java/pkg1/pincode/WebController.java
@@ -1,13 +1,28 @@
 package pkg1.pincode;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class WebController {
+	@Autowired
+	PincodeRepo pr;
 	@GetMapping("/web")
 	public String getDetails() {
 		return "index";
 	}
-	
+	@GetMapping("/fileupload")
+	public String getFileUpload() {
+		return "fileupload";
+	}
+	@PostMapping("/details")
+	public String postDetails(@ModelAttribute PincodeEntity pe, Model model ) {
+		pr.save(pe);
+		model.addAttribute("msg","succesfully inserted");
+		return "details";
+	}
 }
